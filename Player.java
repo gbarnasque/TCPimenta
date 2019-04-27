a07952367b2afb48b761b49e5d8e0c68678407cd
Gustavo Barnasque <gustavo.barnasque@hotmail.com>
2019-04-26 21:48:18 -0300

    Escala com midi, e como criar, alterar instrumentos.

--- Player.java
+++ Player.java
@@ -9,6 +9,7 @@ import javax.sound.midi.*;
 //http://www.music-software-development.com/midi-tutorial.html
 public class Player{
 
+	public static int j;
 	public static void main(String[] args) {
 		//new Player(args[0]);
 		try{
@@ -17,21 +18,49 @@ public class Player{
 			MidiChannel[] mc = syn.getChannels();
 
 			Instrument[] instr = syn.getDefaultSoundbank().getInstruments();
+			// Instrument[] instr = syn.getAvailableInstruments();
 
-			syn.loadInstrument(instr[90]);
+			for(int i=0; i<instr.length; i++){
+				System.out.print(i);
+				System.out.println(instr[i]);
+				//syn.loadInstrument(instr[i]);
+			}
+			
+
+			//syn.loadInstrument(instr[73]);	
 
 			JFrame frame = new JFrame("Sound1");                
-			JPanel pane = new JPanel();                         
-			JButton button1 = new JButton("Click me!");            
-			frame.getContentPane().add(pane);                   
-			pane.add(button1);                                     
+			JPanel pane = new JPanel();
+			frame.getContentPane().add(pane); 
+			JButton[] buttons = new JButton[128];
+
+			//mc[5].programChange(instr[2].getPatch().getProgram());
+			for(j=0;j<1;j++){
+			String jota = Integer.toString(j);
+			buttons[j] = new JButton(jota);            
+			                  
+			pane.add(buttons[j]);                                     
+			mc[7].programChange(instr[0].getPatch().getProgram());
+			//System.out.println(instr[j].getPatch().getProgram());
+			buttons[j].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {  
+					mc[7].noteOn(80,100);
+					/* 
+						Dó - 60
+						Ré - 63
+						Mi - 66
+						Fá - 69
+						Sol - 72
+						Lá - 75
+						Sí - 77
+						Dó - 80
+					*/
+				}
+			});
+			}
 			frame.pack();                                       
 			frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 			frame.setVisible(true);                                       
-			button1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {  
-				mc[5].noteOn(60,600);
-			}});
 		}
 		catch(Exception e){
 			e.printStackTrace();
